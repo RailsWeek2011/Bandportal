691710d2189288ca7098e093f93910321ca671cd
Markus Bader <markus.bader@mni.th-mittelhessen.de>
2011-07-21 10:57:30 +0200

    Basis-Rails hinzugefuegt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bandportal::Application.config.secret_token = '7abcca49bcd7ecc04d002b7068ee95be43300bff5b82c8b71a51b39abe1784d2c6548bb9743c63ebad06185072db41b2737985ab241cb4db4d58efe48b9a6aee'
